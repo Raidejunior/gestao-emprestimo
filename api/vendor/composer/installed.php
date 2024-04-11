d21f2bea4ad878d359e31b63d2c5aec1c490338a
Raide Junior <raidejunior@gmail.com>
2024-04-10 21:09:43 -0300

    Adiciona dependencias iniciais back-end

---
+++ api/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'raide/api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5721521796425086e5c1fb4e4b58126576debfd3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kahlan/kahlan' => array(
+            'pretty_version' => '5.2.6',
+            'version' => '5.2.6.0',
+            'reference' => 'e7d89f4a6ba0d71517a31e7e197b14d3bd85a0af',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kahlan/kahlan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'phputil/router' => array(
+            'pretty_version' => 'v0.2.15',
+            'version' => '0.2.15.0',
+            'reference' => '5dbbb76afeeef076b5663ea92c0005c79887ffb9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phputil/router',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'raide/api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5721521796425086e5c1fb4e4b58126576debfd3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
