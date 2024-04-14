d59fb6a9a08476dc3395bf9a7cbc93dfbf4c4d28
Raide Junior <raidejunior@gmail.com>
2024-04-14 14:24:23 -0300

    Alterações para busca de cliente por CPF

---
+++ client/src/controllers/controladora-cliente.ts
@@ -0,0 +1,28 @@
+import { Cliente } from '../models/Cliente.ts';
+import { VisaoCliente } from '../views/visao-cliente.ts';
+
+export class ControladoraCliente {
+
+    visao: VisaoCliente;
+
+    constructor(){
+        this.visao = new VisaoCliente();
+    }
+
+    configurarBusca(): void {
+        this.visao.definirAcaoAoBuscar(this.buscar.bind(this));
+    }
+
+    async buscar() {
+        const cpf = this.visao.cpf();
+        const c = new Cliente({nome: '', cpf, dataNascimento: new Date()});
+
+        try {
+            const cliente = await c.localizarCliente();
+            this.visao.mostrarResultado(`Olá, ${cliente.nome}!`);
+
+        } catch(e: any) {
+            this.visao.mostrarResultado(e.message);
+        }
+    }
+}
