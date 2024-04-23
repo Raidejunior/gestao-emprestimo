f05aa5a4330e68999b36daa7d9a77f8257ea5f1c
Raide Junior <raidejunior@gmail.com>
2024-04-23 05:43:24 -0300

    test: Implementado teste e2e para busca de cliente

---
+++ client/e2e/utils/buscaCliente.ts
@@ -0,0 +1,11 @@
+import { expect} from "@playwright/test";
+export async function buscaCliente( page, cpf, mensagemEsperada ) {
+   // Acesso a tela de busca de cliente por cpf
+   await page.goto('http://localhost:5173/');
+   // Quando eu informo um valor para o cpf
+   await page.fill('#cpf', cpf);
+   // Clico no botão para buscar cliente por cpf
+   await page.click('#buscar');
+   // Então vejo na tela a mensagem correspondente ao valor do cpf informado.
+   await expect(page.locator('.info-cliente')).toContainText(mensagemEsperada);
+}
