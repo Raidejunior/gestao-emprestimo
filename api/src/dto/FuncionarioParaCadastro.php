073e88f0430ca23cf760a51c97e8e0f233268684
Raide Junior <raidejunior@gmail.com>
2024-08-16 10:41:02 -0300

    refactor: Alterado código para se encaixar no padrão dos testes necessários.

--- api/src/dto/FuncionarioParaCadastro.php
+++ api/src/dto/FuncionarioParaCadastro.php
@@ -1,23 +1,48 @@
 <?php
 namespace src\dto;
 
+use src\utils\AtributosInvalidos;
+
 class FuncionarioParaCadastro {
     public ?string $login;
     public ?string $email;
     public ?string $senha;
     public ?string $permissao;
 
-    public array $atributosInvalidos = [];
+    public AtributosInvalidos $atributosInvalidos;
 
     public function __construct(array $dados) {
-        $this->login = $dados['login'] ?? $this->tratarInvalido('login');
-        $this->email = $dados['email'] ?? $this->tratarInvalido('email') ; 
-        $this->senha = $dados['senha'] ?? $this->tratarInvalido('senha') ;
-        $this->permissao = $dados['permissao'] ?? $this->tratarInvalido('pemissão');
+        $this->atributosInvalidos = new AtributosInvalidos();
+        $this->login = $dados['login'] ?? null;
+        $this->email = $dados['email'] ?? null; 
+        $this->senha = $dados['senha'] ?? null;
+        $this->permissao = $dados['permissao'] ?? null;
+
+        $this->valida();
+    }
+
+    public function chamarConstrutorParaTeste(array $dados = []) {
+        $this->__construct($dados); // Para o construtor novamente apenas para fins de teste.
+    }
+
+    private function valida(): void {
+        // Valida cada atributo
+        if ($this->login === null || $this->login === '') {
+            $this->tratarInvalido('login');
+        }
+        if ($this->email === null || $this->email === '') {
+            $this->tratarInvalido('email');
+        }
+        if ($this->senha === null || $this->senha === '') {
+            $this->tratarInvalido('senha');
+        }
+        if ($this->permissao === null || $this->permissao === '') {
+            $this->tratarInvalido('permissao');
+        }
     }
 
     private function tratarInvalido(string $atributo): void {
-        array_push($this->atributosInvalidos, "O atributo $atributo é obrigatório");
+        $this->atributosInvalidos->adicionar($atributo);
     }
 }
 
